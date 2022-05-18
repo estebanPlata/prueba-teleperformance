191d17ae0dcdd7347107bf9ca48e8c6b36e78662
Esteban <eaplata20@gmail.com>
2022-05-18 07:23:24 -0500

    se configuro el fichero data.service.ts con el metodo get

---
+++ src/app/share/services/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  endPoint: string = 'https://api.github.com/search/users?q=YOUR_NAME';/* se define variable de donde se van a extraer los datos */
+  constructor(private http: HttpClient) {}
+  getData() {/* metodo get para obtener la data*/
+    return this.http.get(this.endPoint);
+  }
+
+}
